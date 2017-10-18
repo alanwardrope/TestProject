5b98e74d940e6b9a6f37da663768d42200f29112
wardropea <awardrope@hotmail.com>
2017-10-18 16:08:36 +0100

    New Files to test ssh connectivity via jsch

---
+++ src/test/java/acme/expedia/tests/SshConnTestCaseChromeHp.java
@@ -0,0 +1,125 @@
+package acme.expedia.tests;
+
+import acme.expedia.framework.common.SshConnector;
+import acme.expedia.framework.pages.MainMenuFactoryHp;
+import acme.expedia.framework.pages.SearchPageFactoryHp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by vanwh on 23/02/2017.
+ */
+public class SshConnTestCaseChromeHp {
+
+    private WebDriver driver;
+    private String baseUrl;
+    private String baseUrl2;
+    SearchPageFactoryHp searchPage;
+    MainMenuFactoryHp menuSelect;
+    SshConnector sshc;
+
+    // DB Connection Parameters
+    static Connection conn = null;
+    // Statement object
+    private static Statement stmt;
+    // Result Set
+    private static ResultSet results = null;
+    // Constant for Database URL
+    public static String SSH_HOST = "ald-eqc-sys-esb";
+    // Constant for server name
+    public static String SSH_USER = "fuse";
+    // Constant for port number
+    public static String SSH_PASSWD = "fuseAdmin";
+    // Constant for Database SID/DBNAME
+    public static String SSH_COMMAND = "./a.sh";
+
+
+    @BeforeClass
+    public void beforeClass() throws IOException {
+
+        // For Chrome Add Options
+        // C:\\users\\vanw\\AppData\\Local\\Google\\Chrome\\User Data\\Default\Extensions\<extension_id>\version_num.crx\
+        //ChromeOptions options = new ChromeOptions();
+        //options.addExtensions(new File("C:\\Users\\vanwh\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\ecnphlgnajanjnkcmbpancdjoidceilk\\2.0.8563_0.crx"));
+        //driver = new ChromeDriver();
+        //baseUrl = "https://www.expedia.com";
+        //searchPage = new SearchPageFactoryHp(driver);
+        //menuSelect = new MainMenuFactoryHp(driver);
+
+
+
+        // Maximize the browser's window
+        //driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.get(baseUrl);
+    }
+
+    @Test
+    public void test() throws IOException {
+        /*
+        */
+        //System.out.println("AM I HERE");
+        //menuSelect.clearHeadersOnStartUp();
+        //System.out.println("AM I HERE NOW");
+        //wait(10000);
+
+        //searchPage.getWhenVisible(By.id("tab-flight-tab-hp"), 20);
+
+        //searchPage.clickFlightsTab();
+
+        // Rotate through the trip buttons
+        //searchPage.selectOneWayTripButton();
+        //searchPage.selectMultiDestButton();
+        //searchPage.selectReturnTripButton();
+
+        // Now enter values for flight
+        //searchPage.setOriginatingCity("New York");
+        //searchPage.setDestinationCity("Chicago");
+        // Hp site - Cannot enter dates into the input field - needs rework
+        //searchPage.setDepartureDate("18/03/2017");
+        //searchPage.clearAndSetReturnDate("25/03/2017");
+
+        //Note once you enter a dept date when you select the rtn date field it automaticall populates it with the next day
+        //String aDate = searchPage.getReturnDate()
+        //System.out.println("The date returned is " + aDate);
+        //searchPage.setReturnDate("25/03/2017");
+
+
+        // Now select number of adults and children
+        //searchPage.setNumberOfAdultsDropdown(2);
+        //searchPage.getNumAdultsDropdown();
+       // searchPage.setNumberOfChildrenDropdown("2");
+        //searchPage.numAdultsDropdown.selectByIndex(2);
+        //searchPage.numChildrenDropdown.selectByVisibleText("6");
+        sshc = new SshConnector(SSH_HOST, SSH_USER, SSH_PASSWD, SSH_COMMAND);
+        // TODO - Need to add condition so it stops in a known time period
+        // TODO - Perhaps have two jobs running on UNIX box one to capture a reponse if erro occurs
+        sshc.openSshConnection();
+        System.out.println("END OF TestClass");
+
+
+    }
+
+
+    @AfterClass
+    public void afterClass() throws IOException {
+
+        System.out.println("In AfterClass");
+        //driver.quit();
+
+
+    }
+}
---
+++ src/test/java/acme/expedia/tests/a.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# ######################################
+# This shell script should be deployed
+# into the fuse user home directory on
+# the ESB server and made executable
+# ######################################
+
+# VARIABLES
+logger=logfile.txt
+logdir=/opt/fuse/data/log/
+logfile=fuseesb-DEBUG.log
+b=0
+
+# DEBUG
+#echo $logdir$logfile
+#echo $homedir/$logger
+
+# Delete previous logger file (if one exists)
+rm -f $homedir/$logger
+# Create a new logger file of size zero bytes
+touch $homedir/$logger
+
+# Invoke the tail on the fuse log to trap the exml payload and output it to the logger file
+tail -f $logdir$logfile | grep Payload | grep CUSTOMER_PORTAL | grep ClaimNotification > $homedir/$logger&
+# Capture the pid of the tail so we can kill it later
+tailpid=$!
+
+# loop in here until logger file is > 0
+# then return the contents in a string`
+chk=0
+while [ $chk -eq 0 ]
+do
+#echo "validating"
+if [ -s $homedir/$logger ]
+then
+ chk=1
+else
+  chk=0
+fi
+# DEBUG
+#echo $chk
+done
+
+# File is greater than 0, now kill the job as it si running in bg mode
+kill $tailpid
+
+# Cat the require text back from the putty session
+cat $logger
